1ef95f6655e31311b1049285d22c42e1e9b43f5f
buitathienan <khapyeup@gmail.com>
2026-07-31 16:11:10 +0700

    feat: update PayrollInfo model to include effective date fields and remove tax-related fields

--- src/payroll/payroll.controller.ts
+++ src/payroll/payroll.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
 import { PayrollService } from './payroll.service';
-import { GeneratePayrollDto } from './dto/generative-payroll.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Payroll')
@@ -8,9 +7,4 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 @ApiBearerAuth()
 export class PayrollController {
   constructor(private payrollService: PayrollService) {}
-
-  @Post()
-  async createPayrollRun(@Body() dto: GeneratePayrollDto, @Req() req: any) {
-    return this.payrollService.draftPayrollRun(dto, req.user.employeeId);
-  }
 }
--- src/payroll/payroll.service.ts
+++ src/payroll/payroll.service.ts
@@ -1,61 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { GeneratePayrollDto } from './dto/generative-payroll.dto';
 import { PrismaService } from 'src/database/prisma.service';
-import { PayType } from 'src/generated/prisma/enums';
 
 @Injectable()
 export class PayrollService {
   constructor(private prisma: PrismaService) {}
-
-  async draftPayrollRun(dto: GeneratePayrollDto, processedById: string) {
-    const employees = await this.prisma.employee.findMany({
-      where: {
-        payrollInfo: {
-          isNot: null,
-        },
-      },
-      include: {
-        payrollInfo: {
-          include: {
-            allowances: true,
-            deductions: true,
-          },
-        },
-      },
-    });
-
-    for (const employee of employees) {
-      const payslip = await this.caculateEmployeePayroll(
-        dto.periodStart,
-        dto.periodEnd,
-        employee.id,
-        employee.payrollInfo?.payType,
-      );
-    }
-  }
-
-  private async caculateEmployeePayroll(
-    periodStart: Date,
-    periodEnd: Date,
-    employeeId: string,
-    payType: PayType | undefined,
-  ) {
-    const attendanceAgg = await this.prisma.attendance.aggregate({
-      where: {
-        date: {
-          gte: periodStart,
-          lte: periodEnd,
-        },
-        employeeId,
-      },
-      _sum: { hoursWorked: true },
-    });
-
-    // If they didn't work any hours, it returns null, fallback to 0
-    const totalWorkedHours = Number(attendanceAgg._sum.hoursWorked) || 0;
-
-    switch (payType) {
-      case 'HOURLY':
-    }
-  }
 }
